56be10e0f10f5f4dbd69adf3174f01dc1bd89751
Naveen Sai <spnaveen675@gmail.com>
2022-03-15 23:31:47 +0530

    Updated

--- script.js
+++ script.js
@@ -1,34 +1,61 @@
-window.addEventListener('load', ()=> {
+window.addEventListener('load',()=>{
 
-    let urlEncoder = document.querySelector('#UrlEncoder');
-    let urlDecoder = document.querySelector('#UrlDecoder');
-    let urlEncodeDecodeInput = document.querySelector('#urlEncodeDecodeIn');
-    let urlEncodeDecodeOutput = document.querySelector('#urlEncodeDecodeOut');
+    let urlEncod = document.querySelector("#UrlEncoder");
+    let urlDecod = document.querySelector("#UrlDecoder");
+    let urlEncodeDecodeInput = document.querySelector("#urlEncodeDecodeIn");
+    let urlEncodeDecodeOutput = document.querySelector("#urlEncodeDecodeOut");
+    let base64EncodeInput = document.querySelector("#base64EncodeIn");
+    let base64Encod = document.querySelector("#base64Encode");
+    let base64EncodeOutput = document.querySelector("#base64EncodeOut");
+    let base64DecodeInput = document.querySelector("#base64DecodeIn");
+    let base64Decod = document.querySelector("#Base64Decode");
+    let base64DecodeOutput = document.querySelector("#base64DecodeOut");
 
-    urlEncoder.addEventListener('click', event => {
-        let encodeValue = encodeURIComponent(urlEncodeDecodeIn.value);
-        urlEncodeDecodeOutput.innerHTML=encodeValue;    
+    
+    
+
+    // console.log(base64Decod);
+    base64Decod.addEventListener('click', () => {
+
+        let baseInput = base64DecodeInput.value;
+        let baseDecodedValue = atob(baseInput);
+        base64DecodeOutput.value = baseDecodedValue;    
     })
+   // console.log(base64Decod);
+    
+    // urlEncod.addEventListener('click', () => {
 
-    urlDecoder.addEventListener('click',event => {
+    //     let encodeValue = encodeURIComponent(urlEncodeDecodeInput.value);
+    //     console.log(encodeValue);
+    //     urlEncodeDecodeOutput.innerHTML=encodeValue;  
+        
+    // })
+
+    urlDecod.addEventListener('click', event => {
 
         let decodeValue = decodeURIComponent(urlEncodeDecodeInput.value);
-        urlEncodeDecodeOutput.innerHTML=decodeValue
+        console.log(decodeValue);
+        urlEncodeDecodeOutput.innerHTML=decodeValue;
     })
 
 
-    let base64EncodeInput = document.querySelector('.base64EncodeIn');
-    let base64Encod = document.querySelector('.base64Encode');
 
     base64Encod.addEventListener('click', event => {
 
-        let input = base64EncodeInput.innerHTML;
-        let encodedValue = Buffer.from(input).toString('base64');
-
-        let output = document.createElement('textarea');
-        output.setAttribute('id','base64EncodeOut');
-        output.innerHTML = encodedValue;
-        document.body.appendChild(output);
+        let baseInput = base64EncodeInput.value;
+        let baseEncodedValue = btoa(baseInput);
+        base64EncodeOutput.value = baseEncodedValue;    
 
     })
+
+    base64Decod.addEventListener('click', event => {
+
+        let baseInput = base64DecodeInput.value;
+        let baseDecodedValue = atob(baseInput);
+        base64DecodeOutput.value = baseDecodedValue;    
+    })
+
+    
+
+
 })
